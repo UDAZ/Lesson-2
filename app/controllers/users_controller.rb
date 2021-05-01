0b9afa772f6c96dc5ccf0f6dad106dd2485fe61b
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-01 07:47:19 +0000

    fix index html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
     @book = Book.new
+    @books = Book.all
   end
 
   def index
